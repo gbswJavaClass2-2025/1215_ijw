fce13f807b162d400f85b9c0c4ba28adb2e203f1
Rebecca_is_alive_ <minehammer26@gmail.com>
2025-08-21 12:18:41 +0900

    그냥 해보고 싶었음

---
+++ src/sec03/exam01/Hello.java
@@ -0,0 +1,13 @@
+package sec03.exam01;
+
+/**
+ * @version 0.0
+ * @author bankai
+ * DATE: 2025-08-21, 12:10 PM
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}

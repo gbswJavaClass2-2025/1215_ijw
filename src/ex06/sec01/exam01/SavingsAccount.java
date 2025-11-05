37281d1b312c25f9616dba5c1d370b7c5320c85b
Rebecca_is_alive_ <minehammer26@gmail.com>
2025-11-05 22:57:20 +0900

    adsf

--- src/ex06/sec01/exam01/SavingsAccount.java
+++ src/ex06/sec01/exam01/SavingsAccount.java
@@ -8,18 +8,13 @@ public class SavingsAccount extends BankAccount{
         this.profit = pro;
     }
 
-    @Override
-    public void deposit(int amount) {
-        balance += amount;
-        System.out.printf("[입금] 금액: %,d 원 | 현재 잔액: %,d 원\n", amount, balance);
-    }
 
     public void deposit(int amount, String reason, boolean i) {
         balance += amount;
         System.out.printf("[입금] 금액: %,d 원 (%s) | 현재 잔액: %,d 원\n", amount, reason, balance);
     }
 
-
+    @Override
     public String getAccountInfo() {
         return String.format("저축 계좌 정보: 계좌 번호: %s | 예금주: %s | 잔액: %,d 원 | 이율: %.1f%% (저축계좌)", number, name, balance, profit);
     }
